9fc685a98cfefed44301f2b62256bb775871d2b6
ellie-ctrl35 <146957037+ellie-ctrl35@users.noreply.github.com>
2024-03-07 02:37:17 +0000

    Add new route for client new request

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'; 
 import Login from './Auth/Login';
+import NewRequest from './Client/NewRequest';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
+
+        <Route path="/client/new-request" element={<NewRequest />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/Client/NewRequest.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './Client.css'
+import Logo from '../resources/Studio.png'
+
+const NewRequest = () => {
+  return (
+    <div className='App'>
+        <div className='the-navbar'>
+            <div className='logo-cont'>
+            <img src={Logo} alt='Studio' />
+            <h1>Data Studio</h1>
+            </div>
+        </div>
+        <div className='the-bottom'>
+            
+        </div>
+    </div>
+  )
+}
+
+export default NewRequest
