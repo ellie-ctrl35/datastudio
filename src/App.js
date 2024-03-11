65203cb7bd9cc83886f5a9943fc774a355a49729
ellie-ctrl35 <146957037+ellie-ctrl35@users.noreply.github.com>
2024-03-11 08:56:53 +0000

    Add Admin Dashboard and All Reports components

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Login from './Auth/Login';
 import NewRequest from './Client/NewRequest';
 import CRequest from './Client/CRequest';
 import NRequest from './Client/NRequest';
+import Admindash from './Admin/Admindash';
+import AllReports from './Admin/AllReports';
 import {AuthContext} from './Context/AuthContext';
 import {useContext,useEffect} from 'react';
 
@@ -21,6 +23,9 @@ function App() {
         <Route path="/client/new-request" element={<NewRequest />} />
         <Route path='/client/requests/approved' element={<CRequest/>}/>
         <Route path='/client/requests/not-approved' element={<NRequest/>}/>
+
+        <Route path="/admin/dashboard" element={<Admindash />} />
+        <Route path="/admin/all-reports" element={<AllReports />} />
       </Routes>
     </BrowserRouter>
   );
