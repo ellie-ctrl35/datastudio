8d7118beeee2fed305119f7ed092dfde3c520bcd
ellie-ctrl35 <146957037+ellie-ctrl35@users.noreply.github.com>
2024-03-11 19:56:17 +0000

    Add navbar and styling to Admin page

--- src/Admin/Admindash.jsx
+++ src/Admin/Admindash.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
 import './Admin.css'
+import { Link } from 'react-router-dom';
+import Logo from '../resources/Studio.png';
+import Avatar from 'react-avatar';
 
 const Admindash = () => {
   return (
-    <div className='App'>Admindash</div>
+    <div className='App'>
+        <div className='the-navbar'>
+            <div className='logo-cont'>
+            <img src={Logo} alt='Studio' />
+            <h1>Data Studio</h1>
+            </div>
+            <div className='navlinks-cont'>
+              <Link className='navlink' to='/client/new-request'>Dashboard</Link>
+              <Link className='navlink' to='/client/requests/approved'>Reports</Link>
+              <Link className='navlink' to='/client/requests/not-approved'>All Requests</Link>
+              <Link className='navlink' to='/client/requests/not-approved'>Add User</Link>
+              <Avatar round name="Emmanuel Nyatepe" size={40}/>
+            </div>
+        </div>
+    </div>
   )
 }
 
--- src/Admin/AllReports.jsx
+++ src/Admin/AllReports.jsx
@@ -2,7 +2,21 @@ import React from 'react'
 
 const AllReports = () => {
   return (
-    <div>AllReports</div>
+    <div>
+      <div className='the-navbar'>
+            <div className='logo-cont'>
+            <img src={Logo} alt='Studio' />
+            <h1>Data Studio</h1>
+            </div>
+            <div className='navlinks-cont'>
+              <Link className='navlink' to='/client/new-request'>Dashboard</Link>
+              <Link className='navlink' to='/client/requests/approved'>Reports</Link>
+              <Link className='navlink' to='/client/requests/not-approved'>All Requests</Link>
+              <Link className='navlink' to='/client/requests/not-approved'>Add User</Link>
+              <Avatar round name="Emmanuel Nyatepe" size={40}/>
+            </div>
+      </div>
+    </div>
   )
 }
 
