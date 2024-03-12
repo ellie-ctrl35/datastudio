8ebbe13b5161d231bf03651e8b9053bbf2ee130b
ellie-ctrl35 <146957037+ellie-ctrl35@users.noreply.github.com>
2024-03-12 23:38:53 +0000

    Add useState and useEffect hooks to AddUser component

--- src/Admin/AddUser.jsx
+++ src/Admin/AddUser.jsx
@@ -1,14 +1,40 @@
-import { useState, useContext } from "react";
+import { useState, useContext ,useEffect} from "react";
 import "./Admin.css";
 import { Link } from "react-router-dom";
 import Logo from "../resources/Studio.png";
 import Avatar from "react-avatar";
 import axios from "axios";
 import { AuthContext } from "../Context/AuthContext";
+import { toast } from "react-toastify";
 
 const AddUser = () => {
   const { userInfo } = useContext(AuthContext);
   const name = userInfo.username;
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [phone, setPhone] = useState("");
+  const [role, setRole] = useState("");
+
+  const createUser = (e) => {
+    e.preventDefault();
+    
+    axios.post("http://localhost:8080/register", { username, email, password, phone, role })
+      .then(res => {
+        console.log(res);
+        if(res.status === 200){
+          toast.success("User added successfully");
+        } else {
+          toast.error("User not added")
+        }
+        e.target[0].value = "";
+        e.target[1].value = "";
+        e.target[2].value = "";
+        e.target[3].value = "";
+        e.target[4].value = "";
+      })
+      .catch(error => console.error("Registration error in InfoContext", error));
+  }
   return (
     <div className="App">
       <div className="the-navbar">
@@ -38,7 +64,23 @@ const AddUser = () => {
 
          </div>
          <div className="userform">
-
+            <h1>Add User</h1>
+            <form onSubmit={createUser}>
+              <label>Username</label>
+              <input onChange={(e)=>setUsername(e.target.value)} type="text" placeholder="Username" />
+              <label>Email</label>
+              <input  onChange={(e)=>setEmail(e.target.value)} type="text" placeholder="Email" />
+              <label>Password</label>
+              <input  onChange={(e)=>setPassword(e.target.value)} type="password" placeholder="Password" />
+              <label>Phone</label>
+              <input  onChange={(e)=>setPhone(e.target.value)} type="text" placeholder="Phone" />
+              <label>Role</label>
+              <select onChange={(e)=>setRole(e.target.value)}>
+                <option value="engineer">Engineer</option>
+                <option value="client">Client</option>
+              </select>
+              <button type="submit">Add User</button>
+            </form>
          </div>
       </div>
     </div>
