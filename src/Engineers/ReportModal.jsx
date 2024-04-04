b5908e9b318264f7d5aae42cc6bf3035f881807c
ellie-ctrl35 <enyatepe.ac@st.vvu.edu.gh>
2024-04-04 12:58:55 +0000

    Official report table issues

--- src/Engineers/ReportModal.jsx
+++ src/Engineers/ReportModal.jsx
@@ -4,6 +4,7 @@ import { jsPDF } from "jspdf";
 import "jspdf-autotable";
 import html2canvas from "html2canvas"; // Import html2canvas
 import "./Modal.css"; // Import the CSS file
+import Logo from '../resources/logo.png';
 
 const ReportModal = ({ report, onClose }) => {
   const [modalContent, setModalContent] = useState("");
@@ -36,13 +37,55 @@ const ReportModal = ({ report, onClose }) => {
       <div className="modal">
         <div className="modal-content" ref={modalRef}>
           <div id="report-details">
-            <h2 style={{ color: "black", fontSize: "2rem" }}>Report Details</h2>
-            <p style={{ color: "black" }}>Report ID: {report._id}</p>
-            <p style={{ color: "black" }}>Facility Name: {report.FacilityName}</p>
-            <p style={{ color: "black" }}>Serial Number: {report.SerialNumber}</p>
-            <p style={{ color: "black" }}>Type: {report.type}</p>
-            <p style={{ color: "black" }}>Work Done: {report.WorkDone}</p>
-            <p style={{ color: "black" }}>Further Works: {report.FurtherWorks}</p>
+            <img src={Logo} alt="Logo" style={{ width: "100px", height: "100px",alignSelf:"center" }} />
+            <table style={{ width: '100%', borderCollapse: 'collapse' }}>
+  <tr>
+    <td style={{ width: '30%' }}>Name of Facility:</td>
+    <td style={{ width: '70%' }}><input type="text" value={report.FacilityName} style={{ width: '100%' }} /></td>
+  </tr>
+  <tr>
+    <td>Equipment Name:</td>
+    <td><input type="text" value={report.EquipmentName} style={{ width: '100%' }} /></td>
+  </tr>
+  <tr>
+    <td>City/Town:</td>
+    <td><input type="text" value={report.CityTown} style={{ width: '100%' }} /></td>
+  </tr>
+  <tr>
+    <td>Facility Asset No:</td>
+    <td><input type="text" value={report.FacilityAssetNo} style={{ width: '100%' }} /></td>
+  </tr>
+  <tr>
+    <td>Model Number:</td>
+    <td><input type="text" value={report.ModelNumber} style={{ width: '100%' }} /></td>
+  </tr>
+  <tr>
+    <td>Serial Number:</td>
+    <td><input type="text" value={report.SerialNumber} style={{ width: '100%' }} /></td>
+  </tr>
+  <tr>
+    <td>Date(s):</td>
+    <td><input type="text" value={report.Date} style={{ width: '100%' }} /></td>
+  </tr>
+  <tr>
+    <td>Problem Description:</td>
+    <td><textarea value={report.ProblemDescription} style={{ width: '100%' }} /></td>
+  </tr>
+  <tr>
+    <td>Work Done:</td>
+    <td><textarea value={report.WorkDone} style={{ width: '100%' }} /></td>
+  </tr>
+  
+  <tr>
+    <td>If Yes, State what needs to be done:</td>
+    <td><textarea value={report.FurtherWorkRequired} style={{ width: '100%' }} /></td>
+  </tr>
+  <tr>
+    <td>Recommendation(s):</td>
+    <td><textarea value={report.Recommendations} style={{ width: '100%' }} /></td>
+  </tr>
+</table>
+
           </div>
           <button onClick={handlePrint}>Print Report</button>
           <button onClick={handleDownloadPDF}>Download PDF</button>
