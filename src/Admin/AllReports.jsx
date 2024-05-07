85d92e232b4ba935678ea85574b1b123087669f3
ellie-ctrl35 <146957037+ellie-ctrl35@users.noreply.github.com>
2024-05-07 16:56:49 +0100

    chore: Add lazy loading for images and improve loading spinner in AllReports.jsx

--- src/Admin/AllReports.jsx
+++ src/Admin/AllReports.jsx
@@ -1,4 +1,4 @@
-import { useState,useEffect ,useContext} from "react";
+import { useState, useEffect, useContext } from "react";
 import { Link } from "react-router-dom";
 import Logo from "../resources/Studio.png";
 import Avatar from "react-avatar";
@@ -6,22 +6,30 @@ import "./Admin.css";
 import axios from "axios";
 import { AuthContext } from "../Context/AuthContext";
 import ReportModal from "./ReportModal";
+import Loader from "react-loader-spinner"; // Import loader
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css"; // Import loader styles
 
 const AllReports = () => {
-  const [requests,setRequests]= useState([]);
+  const [requests, setRequests] = useState([]);
+  const [loading, setLoading] = useState(true);
   const { userInfo } = useContext(AuthContext);
   const name = userInfo.username;
-  const [selectedReport, setSelectedReport] = useState(null); 
-  useEffect(()=>{
-    axios.get("http://localhost:8080/getallreports")
-    .then(res => {
-      console.log(res);
-      setRequests(res.data.data)
-      console.log("Allreport:",requests)
-    })
-    .catch(error => console.error("Error fetching requests", error));
+  const [selectedReport, setSelectedReport] = useState(null);
 
-  },[])
+  useEffect(() => {
+    axios
+      .get("http://localhost:8080/getallreports")
+      .then((res) => {
+        console.log(res);
+        setRequests(res.data.data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error("Error fetching requests", error);
+        setLoading(false);
+      });
+  }, []);
+
   const openModal = (request) => {
     setSelectedReport(request);
   };
@@ -29,6 +37,7 @@ const AllReports = () => {
   const closeModal = () => {
     setSelectedReport(null);
   };
+
   return (
     <div className="App">
       <div className="the-navbar">
@@ -49,22 +58,37 @@ const AllReports = () => {
           <Link className="navlink" to="/admin/add-user">
             Add User
           </Link>
-        <Avatar round name={name} size={40} />
+          <Avatar round name={name} size={40} />
         </div>
       </div>
       <div className="request-container">
-        <ul className="request-list">
-          {requests.map((request) => (
-            <div key={request._id} className="request">
-              <p>{request.Engineer}</p>
-              <p>{request.FacilityName}</p>
-              <p>{request.SerialNumber}</p>
-              <p>{request.type}</p>
-              <p>{request.createdAt}</p>
-              <button className="request-btn" onClick={() => openModal(request)}>View Report</button>
-            </div>
-          ))}
-        </ul>
+        {loading ? (
+          <Loader
+            type="ThreeDots"
+            color="#00BFFF"
+            height={100}
+            width={100}
+            timeout={3000} // 3 secs timeout
+          />
+        ) : (
+          <ul className="request-list">
+            {requests.map((request) => (
+              <div key={request._id} className="request">
+                <p>{request.Engineer}</p>
+                <p>{request.FacilityName}</p>
+                <p>{request.SerialNumber}</p>
+                <p>{request.type}</p>
+                <p>{request.createdAt}</p>
+                <button
+                  className="request-btn"
+                  onClick={() => openModal(request)}
+                >
+                  View Report
+                </button>
+              </div>
+            ))}
+          </ul>
+        )}
       </div>
       {selectedReport && (
         <ReportModal request={selectedReport} onClose={closeModal} />
